abdbbcff24a42760625b7f7cefdba4972eb6d171
Rossi.Hung <red2486@users.noreply.github.com>
2020-10-24 15:42:20 +0800

    7th project practice

---
+++ 7-questions/app.js
@@ -0,0 +1,24 @@
+//using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+    const btn = question.querySelector(".question-btn");
+    // console.log(btn);
+
+    btn.addEventListener("click", function () {
+        // console.log(question);
+
+        questions.forEach(function (item) {
+            if (item !== question) {
+                console.log(item);
+                item.classList.remove("show-text");
+            }
+        });
+
+        question.classList.toggle("show-text");
+    });
+}
+
+);
+
+// traversing the dom
